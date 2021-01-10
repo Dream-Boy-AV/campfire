a310f25ada89fa2dfbe94da47405738c82cb4bd8
Anton Vereschagin <every.thing.chays@gmail.com>
2021-01-10 19:04:08 +0300

    First commit.

---
+++ campfire_1.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 1366, 768
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_icon(pygame.image.load('graphics/textures/icon.png'))
+
+
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
