bf875a7ada14cb9a4b382dfc7f71824f7283b104
Anton Vereschagin <every.thing.chays@gmail.com>
2021-01-10 19:23:17 +0300

    Test commit.

--- campfire_1.py
+++ campfire_1.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     pygame.display.set_icon(pygame.image.load('graphics/textures/icon.png'))
 
+    print('Ура!')
+
 
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
