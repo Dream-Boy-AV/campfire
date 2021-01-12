6aae3d4678a26db1136245d79c2ad3874e5c1cb6
Anton Vereschagin <every.thing.chays@gmail.com>
2021-01-12 19:10:40 +0300

    Level initialization with chip field (chip images are over-expanded).

--- campfire_1.py
+++ campfire_1.py
@@ -12,6 +12,9 @@ def load_image(name):
 
 FPS = 50
 SIZE = WIDTH, HEIGHT = 1366, 768
+save = [line.rstrip('\n') for line in open('save_data.txt', 'r').readlines()]
+chip_names = ['blue', 'green', 'red', 'yellow']
+
 
 def game():
     level_init()
@@ -21,6 +24,7 @@ def game():
                 terminate()
         pygame.display.flip()
         clock.tick(FPS)
+
 
 def level_init():
     bcg = pygame.transform.scale(load_image('textures\\level_bcg.jpg'), SIZE)
@@ -48,10 +52,90 @@ def level_init():
     level = open('levels/type_{}.txt'.format(str(random.randrange(8))), 'r').readlines()
     level = [line.rstrip('\n').split() for line in level]
 
+    gamemode = int(save[0][7:]) % 2
+    if gamemode == 1:
+        cell_image = pygame.transform.scale(load_image('textures\\cell_opened.png'), (77, 77))
+    else:
+        cell_image = pygame.transform.scale(load_image('textures\\cell_closed.png'), (77, 77))
+    cells_init(level, cell_image)
+
+    chip_set(level)
+
+
+def cells_init(level, cell_image):
+    global cells
+    cells = pygame.sprite.Group()
+    y = 62
+    for line in level:
+        x = 451
+        for place in line:
+            if place == '1':
+                cell = pygame.sprite.Sprite(cells)
+                cell.image = cell_image
+                cell.rect = cell.image.get_rect()
+                cell.rect.x, cell.rect.y = x, y
+            x += 81
+        y += 81
+    cells.draw(screen)
+
+
+def chip_set(level):
+    chips = pygame.sprite.Group()
+    chip_list = []
+    ln = 0
+    y = 71
+    for line in level:
+        x = 460
+        list_line = []
+        pl = 0
+        for place in line:
+            if place == '1':
+                chip_image = random.choice(chip_names)
+                if pl > 1 and ln > 1:
+                    cond1 = list_line[pl - 2] == chip_image
+                    cond2 = list_line[pl - 1] == chip_image
+                    cond3 = chip_list[ln - 2][pl] == chip_image
+                    cond4 = chip_list[ln - 1][pl] == chip_image
+                    while cond1 and cond2 or cond3 and cond4:
+                        chip_image = random.choice(chip_names)
+                        cond1 = list_line[pl - 2] == chip_image
+                        cond2 = list_line[pl - 1] == chip_image
+                        cond3 = chip_list[ln - 2][pl] == chip_image
+                        cond4 = chip_list[ln - 1][pl] == chip_image
+                elif pl > 1:
+                    cond1 = list_line[pl - 2] == chip_image
+                    cond2 = list_line[pl - 1] == chip_image
+                    while cond1 and cond2:
+                        chip_image = random.choice(chip_names)
+                        cond1 = list_line[pl - 2] == chip_image
+                        cond2 = list_line[pl - 1] == chip_image
+                elif ln > 1:
+                    cond3 = chip_list[ln - 2][pl] == chip_image
+                    cond4 = chip_list[ln - 1][pl] == chip_image
+                    while cond3 and cond4:
+                        chip_image = random.choice(chip_names)
+                        cond3 = chip_list[ln - 2][pl] == chip_image
+                        cond4 = chip_list[ln - 1][pl] == chip_image
+                list_line += [chip_image]
+                chip_sprite = pygame.sprite.Sprite(chips)
+                chip_sprite.image = \
+                    pygame.transform.smoothscale(load_image('icons\\chip_{}.png'.format(chip_image)),
+                                                 (60, 60))
+                chip_sprite.rect = chip_sprite.image.get_rect()
+                chip_sprite.rect.x, chip_sprite.rect.y = x, y
+            else:
+                list_line += [0]
+            x += 81
+            pl += 1
+        y += 81
+        chip_list += [list_line]
+        ln += 1
+    chips.draw(screen)
+
+
 def terminate():
     pygame.quit()
     sys.exit()
-
 
 
 if __name__ == '__main__':
